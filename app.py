a7a5e49f9620c5ee93a79c26a760236ef5e44755
venkateshpaila <venkateshpaila72@gmail.com>
2026-06-04 15:21:21 +0530

    model and endpoint created

--- app.py
+++ app.py
@@ -2,3 +2,26 @@ import pickle
 from flask import Flask, request, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
+
+app = Flask(__name__)
+# load model
+regmodel = pickle.load(open('regression.pkl','rb'))
+sc = pickle.load(open('scaler1.pkl','rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = sc.transform(np.array(list(data.values())).reshape(1,-1))
+    output  = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+if __name__ == "__main__":
+     app.run(debug=True)    
+print("ALL OK")
